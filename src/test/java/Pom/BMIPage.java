48035351136deaf0475777cbf25a10e0be4a4107
phuonglai99 <44184526+phuonglai99@users.noreply.github.com>
2022-09-08 00:00:20 +0700

    practice with dataprovider in testNG

--- src/test/java/Pom/BMIPage.java
+++ src/test/java/Pom/BMIPage.java
@@ -1,4 +1,46 @@
 package Pom;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class BMIPage {
+    //https://www.calculator.net/bmi-calculator.html
+    private By metricUnits = By.id("menuon");
+    private By clearButton = By.className("clearbtn");
+    private By ageTextArea = By.id("cage");
+    private By genderMale = By.cssSelector("[for='csex1']");
+    private By genderFemale = By.cssSelector("[for='csex2']");
+    private By heightTextArea = By.id("cheightmeter");
+    private By weightTextArea = By.id("ckg");
+    private By calulateButton = By.cssSelector("[value='Calculate']");
+    private By result = By.xpath("//div[@class='bigtext']");
+    private By textErrormsg = By.className("rightresult");
+    private WebDriver driver;
+
+    public BMIPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void clickOnTabMetric(){
+        driver.findElement(metricUnits).click();
+    }
+    public void clickOnClearButton(){
+        driver.findElement(clearButton).click();
+    }
+    public void clickOnCalulateButton(){
+        driver.findElement(calulateButton).click();
+    }
+    public void fillData(String age, String gender, String height, String weight){
+        driver.findElement(ageTextArea).sendKeys(age);
+        if(gender=="Male") driver.findElement(genderMale).click();
+        else driver.findElement(genderFemale).click();
+        driver.findElement(heightTextArea).sendKeys(height);
+        driver.findElement(weightTextArea).sendKeys(weight);
+    }
+
+    public String getResults(){
+        return driver.findElement(result).getText();
+    }
+    public String getErrorMsg(){
+        return driver.findElement(textErrormsg).getText();
+    }
 }
--- src/test/java/TestPom/BMITest.java
+++ src/test/java/TestPom/BMITest.java
@@ -1,4 +1,60 @@
 package TestPom;
 
+import Pom.BMIPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
 public class BMITest {
+    WebDriver driver;
+    BMIPage bmiPage;
+
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void open(){
+        driver.get("https://www.calculator.net/bmi-calculator.html");
+        bmiPage = new BMIPage(driver);
+    }
+    @DataProvider(name = "test-data-invalid")
+    public Object[][] dataInvalid(){
+        return new Object[][]{{"1","Male","120","20","Please provide an age between 2 and 120."},
+                {"3","Female","0","20","Please provide positive height value."},
+                {"2","Male","120","-20","Please provide positive weight value."},
+                {"121","Male","-120","0","Please provide an age between 2 and 120.\n" +
+                        "Please provide positive height value.\n" +
+                        "Please provide positive weight value."}};
+    }
+    public Object[][] dataValid(){
+        return new Object[][]{{"2","","","","",""},
+                {},
+                {}};
+    }
+
+    @Test(dataProvider = "test-data-invalid")
+    public void invalidData(String age, String gender, String height, String weight, String expectedResult){
+        bmiPage.clickOnTabMetric();
+        bmiPage.clickOnClearButton();
+        bmiPage.fillData(age,gender,height,weight);
+        bmiPage.clickOnCalulateButton();
+        Assert.assertEquals(bmiPage.getErrorMsg(),expectedResult);
+    }
+    @Test
+    public void validData(String age, String gender, String height, String weight, String expectedResult){
+        bmiPage.clickOnTabMetric();
+        bmiPage.clickOnClearButton();
+        bmiPage.fillData(age,gender,height,weight);
+        bmiPage.clickOnCalulateButton();
+        Assert.assertEquals(bmiPage.getResults(),expectedResult);
+    }
+    @AfterClass
+    public void close(){
+        driver.close();
+    }
 }
